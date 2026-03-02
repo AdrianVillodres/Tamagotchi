70efddc18667e0e628a5080f31c5616828c0ac5c
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2026-03-02 14:01:34 +0100

    few errors fixed

--- Core/UI/Program.cs
+++ Core/UI/Program.cs
@@ -7,14 +7,14 @@ namespace Tamagotchi.Core.UI
         public static void Main()
         {
             Stats stats = new Stats(100, 100, 0);
-            Cat taiga = new Cat("Taiga", Emotions.happy, stats);
+            Cat taiga = new Cat("Taiga", Emotions.Happy, stats);
             Food healthyBar = new Food("HealthyBar", TypeFood.Snack);
             Food animalFood = new Food("AnimalFood", TypeFood.Meal);
             Item healtyBar = new Item("HealtyBar", healthyBar);
             Item animalFoodI = new Item("AnimalFood", animalFood);
             Player player = new Player(taiga);
 
-
+            UIConfig.GetPetArt(taiga.Emotion.ToString());
         }
     }
 }
